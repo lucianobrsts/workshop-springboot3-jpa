021878e060ef3eee2d09e19c18e0bfee98168cdc
lucianobrsts <lucianobrsts@yahoo.com.br>
2024-05-23 21:49:04 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,6 +2,8 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,13 +15,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
 @NoArgsConstructor
 @Entity
+@EqualsAndHashCode
 @Table(name = "tb_order")
 public class Order implements Serializable {
 
@@ -27,16 +32,23 @@ public class Order implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter @Setter
 	private Long id;
 
 	@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	@Getter @Setter
 	private Instant moment;
 
 	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
+	@Getter @Setter
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	@Getter
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
